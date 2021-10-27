fc401f4fe55dab2677b62c21ab7586d0bb32bdf7
KouT127 <kou26haru@gmail.com>
2021-10-27 23:18:05 +0900

    modify: Remove unnecessary component

--- app/src/main/java/com/example/composeplayground/MainActivity.kt
+++ app/src/main/java/com/example/composeplayground/MainActivity.kt
@@ -3,27 +3,43 @@ package com.example.composeplayground
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import com.example.composeplayground.component.WordsListScreen
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.composeplayground.ui.theme.ComposePlaygroundTheme
-import com.example.composeplayground.viewmodels.WordsListViewModel
+import com.example.composeplayground.ui.wordsList.WordsListScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val viewModel: WordsListViewModel by viewModels()
+
         setContent {
             ComposePlaygroundTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    WordsListScreen(viewModel)
-                }
+                Surface(
+                    color = MaterialTheme.colors.background,
+                    content = @Composable {
+                        val navController = rememberNavController()
+                        NavHost(navController = navController, startDestination = "/") {
+                            composable("/") {
+                                Column() {
+                                    Button(onClick = { navController.navigate("/words") }) {
+                                    }
+                                }
+                            }
+                            composable("/words") {
+                                WordsListScreen()
+                            }
+                        }
+                    }
+                )
             }
         }
     }
 }
-
